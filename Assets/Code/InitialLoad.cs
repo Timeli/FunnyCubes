735b2b664a49ee38feedd4066cf898319efdd81a
Timely <timbelentsov@gmail.com>
2022-10-18 07:01:28 +0300

    finish

---
+++ Assets/Code/InitialLoad.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Code
+{
+    public class InitialLoad : MonoBehaviour
+    {
+        public void Start()
+        {
+            LoadLevel("One");
+        }
+        
+        public void LoadLevel(string scene)
+        {
+            if (SceneManager.GetActiveScene().name == scene)
+                return;
+
+            AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+        }
+    }
+}
